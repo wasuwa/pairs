8d85b855af5aeaa0e882475396dba526eb6e3192
wasuwa <wasuwa911@gmail.com>
2022-09-03 10:41:23 +0900

    env パッケージを main.go から読み込む

--- cmd/footprint/main.go
+++ cmd/footprint/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"pairs/pkg/env"
 	"pairs/pkg/logging"
 	"pairs/pkg/selenium"
 )
@@ -8,6 +9,8 @@ import (
 func main() {
 	logging.Init()
 	defer logging.Sync()
+
+	env.Init()
 
 	s := selenium.InitChrome()
 	defer s.Stop()
--- pkg/env/env.go
+++ pkg/env/env.go
@@ -1,10 +1,16 @@
 package env
 
-import "github.com/joho/godotenv"
+import (
+	"pairs/pkg/logging"
+
+	"github.com/joho/godotenv"
+	"go.uber.org/zap"
+)
 
 // Init 初期化
 func Init() {
 	if err := godotenv.Load("config/.env"); err != nil {
+		logging.Panic("環境変数が読み込めませんでした", zap.Error(err))
 		panic(err)
 	}
 }
